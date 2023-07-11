4de926d46ae002952c07117a4b0c71c640ec9b2e
amyKaDa99 <aminahdakka@gmail.com>
2023-07-11 22:17:20 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api-docs', app, document);
 
 
-await app.listen(process.env.PORT || 3000, "0.0.0.0");
+await app.listen(process.env.PORT || 5500);
 
 }
 bootstrap();
